8d782578bfdbff6999db6ddeb47ca7cde7b829ef
Ecoder-JianZhou <ecoder-zj@outlook.com>
2024-03-06 19:38:28 -0500

    UPDATE RUN DESKTOP

---
+++ run_desktop.sh
@@ -0,0 +1,10 @@
+nohup .//run_desktop_P04.sh > outlogs/0224/out_P04.log &
+nohup ./run_desktop_P06.sh > outlogs/0224/out_P06.log &
+nohup ./run_desktop_P08.sh > outlogs/0224/out_P08.log &
+nohup ./run_desktop_P10.sh > outlogs/0224/out_P10.log &
+nohup ./run_desktop_P11.sh > outlogs/0224/out_P11.log &
+nohup ./run_desktop_P13.sh > outlogs/0224/out_P13.log &
+nohup ./run_desktop_P16.sh > outlogs/0224/out_P16.log &
+nohup ./run_desktop_P17.sh > outlogs/0224/out_P17.log &
+nohup ./run_desktop_P19.sh > outlogs/0224/out_P19.log &
+nohup ./run_desktop_P20.sh > outlogs/0224/out_P20.log &
